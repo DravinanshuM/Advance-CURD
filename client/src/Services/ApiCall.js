51737ba6e055ac4f06b104ff5830a30dcac5b938
DravinanshuM <mishradravinanshu@gmail.com>
2024-07-05 21:01:11 +0530

    add

--- client/src/Services/ApiCall.js
+++ client/src/Services/ApiCall.js
@@ -1,21 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
-const commonRequest = async(methods, url, body, header) => {
-    const config = {
-        method: methods,
-        url: url,
-        data: body,
-        headers: header ? header:{
-            "Content-Type": "Application/json"
-        } 
-    }
+const commonRequest = async (methods, url, body, header) => {
+  const config = {
+    method: methods,
+    url: url,
+    data: body,
+    headers: header
+      ? header
+      : {
+          "Content-Type": "Application/json",
+        },
+  };
 
-    // create axios instance.
-    return axios(config).then((data)=>{
-        return data;
-    }).catch((error)=>{
-        return error;
+  // create axios instance.
+  return axios(config)
+    .then((data) => {
+      return data;
     })
-}
+    .catch((error) => {
+      return error;
+    });
+};
 
-export { commonRequest }
+export { commonRequest };
